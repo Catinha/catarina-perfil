6f8335c1976069725b912c8716322f913b179dd6
Catinha <92885066+Catinha@users.noreply.github.com>
2022-03-14 22:36:51 -0300

    Delete Site Pessoal usando Jquery e Bootstrap - Catarina directory

--- Site Pessoal usando Jquery e Bootstrap - Catarina/footerContent.php
+++
@@ -1,3 +0,0 @@
-<div id = "banner">
-      <img height="60px" src = "img/footer 3 (1).png">
-</div>
--- Site Pessoal usando Jquery e Bootstrap - Catarina/section02Content.php
+++
@@ -1,5 +0,0 @@
-<div id="sobre_mim"> 
-
-<img height="500px" width="685px" src = "img/conheça um pouco sobre mim....png">
-
-</div>
